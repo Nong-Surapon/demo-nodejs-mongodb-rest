4198ba9cc11b807addf19eefe242ae9517d85475
Surapon <modernlife1988@gmail.com>
2022-12-06 08:13:50 +0700

    updated file

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var routes = require('./routes/routes');
 
 var app = express();
 
-// view engine setup
+// view engine setup //Jenkins
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
